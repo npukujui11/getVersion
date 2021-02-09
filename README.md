3b7fae1ed5f888b7be79dedc736b516e398065d6
Ku Jui <npukujui11@gmail.com>
2021-02-09 09:19:42 +0800

    demo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # getVersion
+用于从浏览器获取网页中某个字段的一段python代码
---
+++ getVersion.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+# By Ku Jui
+import requests
+from bs4 import BeautifulSoup
+hdrs = {'User-Agent':'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)'}
+url = "http://10.5.7.39/polarion/#/project/EAGLEII/workitems/softwareUnit"
+r = requests.get(url, headers = hdrs)
+soup = BeautifulSoup(r.content.decode('utf8', 'ignore'), 'lxml')
+#定义变量
+tbody = "empty"
+tbody = soup.find('div')
+#获取SVN版本号
+#for tag in soup.find_all('td', id_='FIELD_SVNversion'):
+#    subversion = tag.fing('div').get_text()
+#    nums = subversion
+print(tbody)
